87a1e2a7010371af41214bee1e18de708761498a
Shcherbachenya Igor <evgenii-onegin91@mail.ru>
2020-07-19 00:53:25 +0300

    CI/CD fixes

--- cicd/deploy.sh
+++ cicd/deploy.sh
@@ -20,10 +20,10 @@ touch ~/.ssh/known_hosts
 echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
 
 echo -e "copying application $CONSOLE_NAME to $SSH_SERVER"
-scp -i ${SSH_KEY_FILE} ${CONSOLE_NAME}/build/${CONSOLE_NAME}.zip ${SSH_USER}@${SSH_SERVER}:/tmp/${CONSOLE_NAME}.zip
+scp -i ${SSH_KEY_FILE} ../${CONSOLE_NAME}/build/${CONSOLE_NAME}.zip ${SSH_USER}@${SSH_SERVER}:/tmp/${CONSOLE_NAME}.zip
 
 echo -e "copying application $INTEGRATION_NAME to $SSH_SERVER"
-scp -i ${SSH_KEY_FILE} ${INTEGRATION_NAME}/build/${INTEGRATION_NAME}.zip ${SSH_USER}@${SSH_SERVER}:/tmp/${INTEGRATION_NAME}.zip
+scp -i ${SSH_KEY_FILE} ../${INTEGRATION_NAME}/build/${INTEGRATION_NAME}.zip ${SSH_USER}@${SSH_SERVER}:/tmp/${INTEGRATION_NAME}.zip
 
 echo -e "copying application $SERVICE_NAME to $SSH_SERVER"
-scp -i ${SSH_KEY_FILE} ${SERVICE_NAME}/libs/build/${SERVICE_NAME}.war ${SSH_USER}@${SSH_SERVER}:/tmp/${SERVICE_NAME}.war
+scp -i ${SSH_KEY_FILE} ../${SERVICE_NAME}/libs/build/${SERVICE_NAME}.war ${SSH_USER}@${SSH_SERVER}:/tmp/${SERVICE_NAME}.war
