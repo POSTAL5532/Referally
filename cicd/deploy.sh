b8f502c681aa3d41a7be14c7a02890b9537413c1
Shcherbachenya Igor <evgenii-onegin91@mail.ru>
2020-07-19 00:44:22 +0300

    CI/CD fixes

--- cicd/deploy.sh
+++ cicd/deploy.sh
@@ -7,14 +7,23 @@ SERVICE_NAME="referally-service"
 
 # To increase safety, you could define this variables as environmental variables on gitlab
 SSH_USER="root"
-SSH_KEY="cicd/id_rsa"
+SSH_KEY_FILE="cicd/id_rsa"
 SSH_SERVER="46.101.148.211"
 
+# setup ssh private key
+# echo "$SSH_KEY_FILE" > "/tmp/id_rsa"
+# SSH_KEY_FILE="/tmp/id_rsa"
+chmod 600 ${SSH_KEY_FILE}
+
+mkdir -p ~/.ssh/
+touch ~/.ssh/known_hosts
+echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
+
 echo -e "copying application $CONSOLE_NAME to $SSH_SERVER"
-scp -i ${SSH_KEY} ${CONSOLE_NAME}/build/${CONSOLE_NAME}.zip ${SSH_USER}@${SSH_SERVER}:/tmp/${CONSOLE_NAME}.zip
+scp -i ${SSH_KEY_FILE} ${CONSOLE_NAME}/build/${CONSOLE_NAME}.zip ${SSH_USER}@${SSH_SERVER}:/tmp/${CONSOLE_NAME}.zip
 
 echo -e "copying application $INTEGRATION_NAME to $SSH_SERVER"
-scp -i ${SSH_KEY} ${INTEGRATION_NAME}/build/${INTEGRATION_NAME}.zip ${SSH_USER}@${SSH_SERVER}:/tmp/${INTEGRATION_NAME}.zip
+scp -i ${SSH_KEY_FILE} ${INTEGRATION_NAME}/build/${INTEGRATION_NAME}.zip ${SSH_USER}@${SSH_SERVER}:/tmp/${INTEGRATION_NAME}.zip
 
 echo -e "copying application $SERVICE_NAME to $SSH_SERVER"
-scp -i ${SSH_KEY} ${SERVICE_NAME}/libs/build/${SERVICE_NAME}.war ${SSH_USER}@${SSH_SERVER}:/tmp/${SERVICE_NAME}.war
+scp -i ${SSH_KEY_FILE} ${SERVICE_NAME}/libs/build/${SERVICE_NAME}.war ${SSH_USER}@${SSH_SERVER}:/tmp/${SERVICE_NAME}.war
