0247d7dfcb66284c49821e6a3fdc0d6bc1b329c2
Shcherbachenya Igor <evgenii-onegin91@mail.ru>
2020-07-20 02:50:43 +0300

    CI/CD fixes

--- cicd/deploy.sh
+++ cicd/deploy.sh
@@ -18,11 +18,11 @@ echo -e "Host *\n\tStrictHostKeyChecking no\n\n" >~/.ssh/config
 
 echo -e "copying application $CONSOLE_NAME to $SSH_SERVER"
 ssh -i ${SSH_KEY_FILE} ${SERVER_FULL} 'rm -r ../opt/referally_clients/referally-console/*'
-scp -i ${SSH_KEY_FILE} -r ${CONSOLE_NAME}/build/dist/. ${SERVER_FULL}:${CLIENTS_SERVER_DIR}/${CONSOLE_NAME}
+scp -i ${SSH_KEY_FILE} -r ${CONSOLE_NAME}/build/dist/* ${SERVER_FULL}:${CLIENTS_SERVER_DIR}/${CONSOLE_NAME}
 
 echo -e "copying application $INTEGRATION_NAME to $SSH_SERVER"
 ssh -i ${SSH_KEY_FILE} ${SERVER_FULL} 'rm -r ../opt/referally_clients/referally-integration/*'
-scp -i ${SSH_KEY_FILE} -r ${INTEGRATION_NAME}/build/dist/. ${SERVER_FULL}:${CLIENTS_SERVER_DIR}/${INTEGRATION_NAME}
+scp -i ${SSH_KEY_FILE} -r ${INTEGRATION_NAME}/build/dist/* ${SERVER_FULL}:${CLIENTS_SERVER_DIR}/${INTEGRATION_NAME}
 
 echo -e "copying application $SERVICE_NAME to $SSH_SERVER"
 scp -i ${SSH_KEY_FILE} ${SERVICE_NAME}/build/libs/${SERVICE_NAME}.war ${SERVER_FULL}:/opt/tomcat/webapps/${SERVICE_NAME}.war
