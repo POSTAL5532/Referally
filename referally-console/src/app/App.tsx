f97ad2785701801d01eb949bff6fedbe445e8b71
Shcherbachenya Igor <evgenii-onegin91@mail.ru>
2020-07-20 03:25:29 +0300

    Добавление ANTD. Stub-страницы

--- referally-console/src/app/App.tsx
+++ referally-console/src/app/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import 'antd/dist/antd.less';
+import {PageHeader, Result} from "antd";
 
 const App = () => {
     return (
-        <h1>Hello Console!</h1>
+        <>
+            <PageHeader title="Referally" subTitle="console application"/>
+            <Result
+                status="404"
+                title="Hmmm....."
+                subTitle="UFO flew in and stole content."
+            />
+        </>
     );
 };
 
