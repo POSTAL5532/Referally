db000a22a91f7d35c9415bf7bcfc132965d34fee
Shcherbachenya Igor <evgenii-onegin91@mail.ru>
2020-07-20 04:01:30 +0300

    Доработка deploy.sh

--- cicd/deploy.sh
+++ cicd/deploy.sh
@@ -5,6 +5,7 @@ INTEGRATION_NAME="referally-integration"
 CLIENTS_SERVER_DIR="/opt/referally_clients/"
 SERVICE_NAME="referally-service"
 
+echo -e "########## Deploying config ..."
 SSH_USER="root"
 SSH_KEY_FILE="cicd/id_rsa"
 SSH_SERVER="46.101.148.211"
@@ -12,18 +13,22 @@ SERVER_FULL=${SSH_USER}@${SSH_SERVER}
 
 chmod 600 ${SSH_KEY_FILE}
 
+echo -e "########## Server accessing"
 mkdir -p ~/.ssh/
 touch ~/.ssh/known_hosts
 echo -e "Host *\n\tStrictHostKeyChecking no\n\n" >~/.ssh/config
 
-echo -e "copying application $CONSOLE_NAME to $SSH_SERVER"
+echo -e "########## Clear old builds of '$CONSOLE_NAME'"
 ssh -i ${SSH_KEY_FILE} ${SERVER_FULL} 'rm -r ../opt/referally_clients/referally-console/*'
+echo -e "########## Copying '$CONSOLE_NAME' to '$SSH_SERVER'"
 scp -i ${SSH_KEY_FILE} -r ${CONSOLE_NAME}/build/dist/* ${SERVER_FULL}:${CLIENTS_SERVER_DIR}/${CONSOLE_NAME}
 
-echo -e "copying application $INTEGRATION_NAME to $SSH_SERVER"
+echo -e "########## Clear old builds of '$INTEGRATION_NAME'"
 ssh -i ${SSH_KEY_FILE} ${SERVER_FULL} 'rm -r ../opt/referally_clients/referally-integration/*'
+echo -e "########## Copying '$INTEGRATION_NAME' to '$SSH_SERVER'"
 scp -i ${SSH_KEY_FILE} -r ${INTEGRATION_NAME}/build/dist/* ${SERVER_FULL}:${CLIENTS_SERVER_DIR}/${INTEGRATION_NAME}
 
-echo -e "copying application $SERVICE_NAME to $SSH_SERVER"
+echo -e "########## Copying '$SERVICE_NAME' to '$SSH_SERVER'"
 scp -i ${SSH_KEY_FILE} ${SERVICE_NAME}/build/libs/${SERVICE_NAME}.war ${SERVER_FULL}:/opt/tomcat/webapps/${SERVICE_NAME}.war
+echo -e "########## restart Tomcat ..."
 ssh -i ${SSH_KEY_FILE} ${SERVER_FULL} 'systemctl restart tomcat'
