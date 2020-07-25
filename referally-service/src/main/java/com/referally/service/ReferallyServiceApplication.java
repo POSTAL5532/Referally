a626742c6d9783903a89c9811ccbc2d71b8adef2
Shcherbachenya Igor <evgenii-onegin91@mail.ru>
2020-07-25 22:20:33 +0300

    Fix service build/deploy

--- referally-service/src/main/java/com/referally/service/ReferallyServiceApplication.java
+++ referally-service/src/main/java/com/referally/service/ReferallyServiceApplication.java
@@ -4,13 +4,20 @@ import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.referally.data.repository")
 @EntityScan(basePackages = "com.referally.data.model")
-public class ReferallyServiceApplication {
+public class ReferallyServiceApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ReferallyServiceApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ReferallyServiceApplication.class, args);
