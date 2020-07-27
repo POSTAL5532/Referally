153518db89a4c9c9f230b20d5b3ccaaf6721e1f7
Shcherbachenya Igor <evgenii-onegin91@mail.ru>
2020-07-27 03:22:07 +0300

    change sh paths variables

--- cicd/deploy.sh
+++ cicd/deploy.sh
@@ -2,7 +2,8 @@
 
 CONSOLE_NAME="referally-console"
 INTEGRATION_NAME="referally-integration"
-CLIENTS_SERVER_DIR="/opt/referally_clients/"
+CONSOLE_SERVER_DIR="/opt/referally_clients/${CONSOLE_NAME}"
+INTEGRATION_SERVER_DIR="/opt/referally_clients/${INTEGRATION_NAME}"
 SERVICE_NAME="referally-service"
 
 echo -e "########## Deploying config ..."
@@ -30,14 +31,14 @@ ssh -i ${SSH_KEY_FILE} ${SERVER_FULL} mysql -u ${SQL_USER} -p${SQL_PASSWORD} ${S
 ssh -i ${SSH_KEY_FILE} ${SERVER_FULL} mysql -u ${SQL_USER} -p${SQL_PASSWORD} ${SQL_DB_NAME} <referally-data/src/data/addTestData.sql
 
 echo -e "########## Clear old builds of '$CONSOLE_NAME'"
-ssh -i ${SSH_KEY_FILE} ${SERVER_FULL} 'rm -r ../opt/referally_clients/referally-console/*'
+ssh -i ${SSH_KEY_FILE} ${SERVER_FULL} "rm -r ..${CONSOLE_SERVER_DIR}/*"
 echo -e "########## Copying '$CONSOLE_NAME' to '$SSH_SERVER'"
-scp -i ${SSH_KEY_FILE} -r ${CONSOLE_NAME}/build/dist/* ${SERVER_FULL}:${CLIENTS_SERVER_DIR}/${CONSOLE_NAME}
+scp -i ${SSH_KEY_FILE} -r ${CONSOLE_NAME}/build/dist/* ${SERVER_FULL}:${CONSOLE_SERVER_DIR}
 
 echo -e "########## Clear old builds of '$INTEGRATION_NAME'"
-ssh -i ${SSH_KEY_FILE} ${SERVER_FULL} 'rm -r ../opt/referally_clients/referally-integration/*'
+ssh -i ${SSH_KEY_FILE} ${SERVER_FULL} "rm -r ..${INTEGRATION_SERVER_DIR}/*"
 echo -e "########## Copying '$INTEGRATION_NAME' to '$SSH_SERVER'"
-scp -i ${SSH_KEY_FILE} -r ${INTEGRATION_NAME}/build/dist/* ${SERVER_FULL}:${CLIENTS_SERVER_DIR}/${INTEGRATION_NAME}
+scp -i ${SSH_KEY_FILE} -r ${INTEGRATION_NAME}/build/dist/* ${SERVER_FULL}:${INTEGRATION_SERVER_DIR}
 
 echo -e "########## Copying '$SERVICE_NAME' to '$SSH_SERVER'"
 scp -i ${SSH_KEY_FILE} ${SERVICE_NAME}/build/libs/${SERVICE_NAME}.war ${SERVER_FULL}:/opt/tomcat/webapps/${SERVICE_NAME}.war
